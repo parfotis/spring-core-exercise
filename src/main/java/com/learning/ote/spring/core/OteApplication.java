04e6eab34533feee6fdc6fb44a2c9620ccb4e243
Stelios Matthaios <stelios.matthaios@gmail.com>
2020-03-12 12:12:50 +0200

    Initial commit

---
+++ src/main/java/com/learning/ote/spring/core/OteApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ote.spring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OteApplication.class, args);
+    }
+
+}
