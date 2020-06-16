caa554f5e7bfdbacb1c0a8fb090ad502053f3737
stelios.matthaios <stelios.matthaios@camelotglobal.com>
2020-06-16 14:47:06 +0300

    [3_3-Dependency-Injection-with-Spring-Annotation] - Add annotations for bean autowiring and xml configuration for component scan

--- src/main/java/com/learning/ote/spring/core/travel/AutowiredInjectedTravel.java
+++ src/main/java/com/learning/ote/spring/core/travel/AutowiredInjectedTravel.java
@@ -1,29 +1,31 @@
 package com.learning.ote.spring.core.travel;
 
 import com.learning.ote.spring.core.vehicle.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+//This annotation is needed to tell spring that this is a bean
+@Component
 public class AutowiredInjectedTravel {
 
     //This is the dependency
+    //No setter or constructor is needed
+    @Autowired
     private Vehicle vehicle;
-
-    public void setVehicle(Vehicle vehicle) {
-        //To Autowire byType or byName I need to have at my class a public setter method
-        this.vehicle = vehicle;
-        System.out.println("Using Setter Injection - Autowired");
-    }
 
     public void startJourney() {
         vehicle.start();
     }
 
-    //To Autowire by Constructor I need to have at my class the appropriate public constructor
     //Autowire by constructor will be preferred by Spring if both setter and constructor are present
-    /*
-    public AutowiredInjectedTravel(Vehicle vehicle) {
-        this.vehicle = vehicle;
-        System.out.println("Using Constructor Injection - Autowired");
-    }
-    */
+//    public AutowiredInjectedTravel(Vehicle vehicle) {
+//        this.vehicle = vehicle;
+//        System.out.println("Using Constructor Injection - Autowired");
+//    }
+
+//    public void setVehicle(Vehicle vehicle) {
+//        this.vehicle = vehicle;
+//        System.out.println("Using Setter Injection - Autowired");
+//    }
 
 }
--- src/main/java/com/learning/ote/spring/core/travel/ConstructorInjectedTravel.java
+++ src/main/java/com/learning/ote/spring/core/travel/ConstructorInjectedTravel.java
@@ -1,13 +1,17 @@
 package com.learning.ote.spring.core.travel;
 
 import com.learning.ote.spring.core.vehicle.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ConstructorInjectedTravel {
 
     //This is the dependency
     private Vehicle vehicle;
 
     //I'm injecting the dependency with through the constructor
+    @Autowired
     public ConstructorInjectedTravel(Vehicle vehicle) {
         this.vehicle = vehicle;
         System.out.println("Using Constructor Injection");
